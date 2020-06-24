c7dfdd51e2248339e033c5bfd14bb19e7721d4ab
Karen Astell <KarenAstell@gmail.com>
2020-06-24 14:02:29 -0400

    started orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,26 @@
 const connection = require("./connection");
 
+// selectAll()
+// insertOne();
+// updateOne();
+
+const orm = {
+  selectAll: (table) => {
+    return new Promise((resolve, reject) => {
+      connection.query(`SELECT * FROM ??`, [table], (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      });
+    });
+  },
+  insertOne: (columnName, )=>{
+      return new Promise((resolve, reject)=>{
+          connection.query(`INSERT INTO ?? (??) VALUES (?)`, [], (err)=>{
+              if(err) reject(err);
+            //   resolve(result);
+          })
+      })
+  }
+};
 
 module.exports = orm;
