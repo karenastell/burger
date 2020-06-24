29bf64c6f437e1ce7e526c78735159f0bd3f4a55
Karen Astell <KarenAstell@gmail.com>
2020-06-24 14:07:43 -0400

    created all orm functions with promises

--- config/orm.js
+++ config/orm.js
@@ -13,14 +13,30 @@ const orm = {
       });
     });
   },
-  insertOne: (columnName, )=>{
-      return new Promise((resolve, reject)=>{
-          connection.query(`INSERT INTO ?? (??) VALUES (?)`, [], (err)=>{
-              if(err) reject(err);
-            //   resolve(result);
-          })
-      })
-  }
+  insertOne: (table, columnName, value) => {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        `INSERT INTO ?? (??) VALUES (?)`,
+        [table, columnName, value],
+        (err) => {
+          if (err) reject(err);
+          console.log(result);
+
+          //   resolve(result);
+        }
+      );
+    });
+  },
+  updateOne: (table, columnName, value, id) => {
+    return new Promise((resolve, reject) => {
+      connection.query(`UPDATE ?? SET ?? = ? WHERE id = ?`),
+        [table, columnName, value, id],
+        (err, result) => {
+          if (err) reject(err);
+          resolve(result);
+        };
+    });
+  },
 };
 
 module.exports = orm;
