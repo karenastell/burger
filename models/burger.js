d5bb7a9ee7600d02f2242f9d503c043b183c9c79
Karen Astell <KarenAstell@gmail.com>
2020-06-24 14:20:07 -0400

    started burger.js - added orm.functions with table specifics - just skeleton at this point

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,13 @@
 const orm = require("../config/orm");
 
+const burger = {
+
+    orm.selectAll("burgers")
+
+    orm.insertOne("burgers", "burger_name", req.body);
+
+    orm.updateOne("burgers", "devoured", req.body, id)
+   
+}
+
 module.exports = burger;
