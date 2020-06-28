171795d7e65e686f5aa8f6ac78904e8809c1599a
Karen Astell <KarenAstell@gmail.com>
2020-06-28 12:40:09 -0400

    set up heroku

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,23 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-// creates the connection
+// creates the connection to Heroku or localhost 8080 to access database
 
-const PORT = process.env.PORT || 8080;
-
-const connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
+}
+
+
+
+
 
 connection.connect((err) => {
   if (err) throw err;
